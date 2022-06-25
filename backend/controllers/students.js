e428c262645578a0c9a35fbf0ee8e36c71f7eec9
Subroto Biswas <smartcoder500@gmail.com>
2022-06-25 08:05:37 +0530

    Server Side Pagination + React Table implemention

--- backend/controllers/students.js
+++ backend/controllers/students.js
@@ -6,7 +6,7 @@ exports.getStudents = async (req, res, next) => {
     let query = Student.find(req.query);
     // Pagination
     const page = req.query.page * 1 || 0;
-    const limit = req.query.limit * 1 || 10;
+    const limit = req.query.limit * 1 || (await Student.find().count());
     const skip = page * limit;
     const pageCount = Math.ceil((await Student.find().count()) / limit);
     query = query.skip(skip).limit(limit);
--- frontend/src/componets/basic-table.js
+++ frontend/src/componets/basic-table.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import {
   createTable,
   useTableInstance,
@@ -6,90 +6,116 @@ import {
   getPaginationRowModel,
 } from "@tanstack/react-table";
 import STUDENTS from "../students.json";
+import axios from "axios";
 
 const table = createTable();
-const defaultData = [...STUDENTS];
-const defaultColumns = [
-  table.createGroup({
-    header: "Full Name",
-    columns: [
-      table.createDataColumn("firstName", {
-        id: "First Name",
-      }),
-      table.createDataColumn("middleName", {
-        id: "Middle Name",
-      }),
-      table.createDataColumn("lastName", {
-        id: "Last Name",
-      }),
-    ],
-  }),
-  table.createDataColumn("age", {
-    id: "Age",
-  }),
-  // table.createGroup({
-  //   header: "Phone Number",
-  //   columns: [
-  //     table.createDataColumn((row) => row.phone[1], {
-  //       id: "Phone Number 1",
-  //     }),
-  //     table.createDataColumn((row) => row.phone[2], {
-  //       id: "Phone Number 2",
-  //     }),
-  //   ],
-  // }),
-  table.createDataColumn("email", {
-    id: "E-mail Address",
-  }),
-  table.createGroup({
-    header: "Full Address",
-    columns: [
-      table.createDataColumn((row) => row.address.street, {
-        id: "Street",
-      }),
-      table.createDataColumn((row) => row.address.city, {
-        id: "City",
-      }),
-      table.createDataColumn((row) => row.address.state, {
-        id: "Address",
-      }),
-      table.createDataColumn((row) => row.address.pincode, {
-        id: "Pin Code",
-      }),
-    ],
-  }),
-  table.createGroup({
-    header: "Date Details",
-    columns: [
-      table.createDataColumn("date_of_birth", {
-        id: "Date of Birth",
-        cell: (props) => new Date(props.getValue()).toDateString(),
-      }),
-      table.createDataColumn("date_of_admission", {
-        id: "Date of Admission",
-        cell: (props) => new Date(props.getValue()).toDateString(),
-      }),
-    ],
-  }),
-];
+// const defaultData = [...STUDENTS];
+
 const BasicTable = () => {
-  const [data] = useState([...defaultData]);
+  const defaultColumns = useMemo(() => [
+    table.createGroup({
+      header: "Full Name",
+      columns: [
+        table.createDataColumn("firstName", {
+          id: "First Name",
+        }),
+        table.createDataColumn("middleName", {
+          id: "Middle Name",
+        }),
+        table.createDataColumn("lastName", {
+          id: "Last Name",
+        }),
+      ],
+    }),
+    table.createDataColumn("age", {
+      id: "Age",
+    }),
+    // table.createGroup({
+    //   header: "Phone Number",
+    //   columns: [
+    //     table.createDataColumn((row) => row.phone[1], {
+    //       id: "Phone Number 1",
+    //     }),
+    //     table.createDataColumn((row) => row.phone[2], {
+    //       id: "Phone Number 2",
+    //     }),
+    //   ],
+    // }),
+    table.createDataColumn("email", {
+      id: "E-mail Address",
+    }),
+    table.createGroup({
+      header: "Full Address",
+      columns: [
+        table.createDataColumn((row) => row.address.street, {
+          id: "Street",
+        }),
+        table.createDataColumn((row) => row.address.city, {
+          id: "City",
+        }),
+        table.createDataColumn((row) => row.address.state, {
+          id: "Address",
+        }),
+        table.createDataColumn((row) => row.address.pincode, {
+          id: "Pin Code",
+        }),
+      ],
+    }),
+    table.createGroup({
+      header: "Date Details",
+      columns: [
+        table.createDataColumn("date_of_birth", {
+          id: "Date of Birth",
+          cell: (props) => new Date(props.getValue()).toDateString(),
+        }),
+        table.createDataColumn("date_of_admission", {
+          id: "Date of Admission",
+          cell: (props) => new Date(props.getValue()).toDateString(),
+        }),
+      ],
+    }),
+  ]);
+  const [data, setData] = useState([]);
   const [columns] = useState([...defaultColumns]);
-  const [pagination, setPagination] = useState({
+  const [{ pageIndex, pageSize }, setPagination] = useState({
     pageIndex: 0,
     pageSize: 10,
   });
+
+  const loadData = async () => {
+    const { data } = await axios.get(
+      `/api/v1/students?page=${pageIndex}&limit=${pageSize}`
+    );
+    setData(data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, [pageIndex, pageSize]);
+
+  const defaultData = React.useMemo(() => [], []);
+
+  const pagination = React.useMemo(
+    () => ({
+      pageIndex,
+      pageSize,
+    }),
+    [pageIndex, pageSize]
+  );
   const instance = useTableInstance(table, {
-    data,
+    data: data?.data ?? defaultData,
     columns,
+    pageCount: data?.pageCount ?? -1,
     state: {
       pagination,
     },
     onPaginationChange: setPagination,
     getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
+    manualPagination: true,
+    // getPaginationRowModel: getPaginationRowModel(),
   });
-  console.log(instance.getRowModel());
+
+  console.log("hello");
   return (
     <div>
       <table border={1}>
@@ -171,11 +197,13 @@ const BasicTable = () => {
             value={instance.getState().pagination.pageSize}
             onChange={(e) => instance.setPageSize(Number(e.target.value))}
           >
-            {[1, 2, 3, 5, 10].map((pageSize) => (
-              <option key={pageSize} value={pageSize}>
-                show {pageSize}
-              </option>
-            ))}
+            {[5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 100].map(
+              (pageSize) => (
+                <option key={pageSize} value={pageSize}>
+                  show {pageSize}
+                </option>
+              )
+            )}
           </select>
         </span>
       </div>
